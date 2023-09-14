2aa68ce1977f913484a1c7b80359f1a1376e26eb
buket.algan <buket.algan@solvoyo.com>
2023-09-14 10:35:05 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import './App.css';
+import BingoGame from './components/BingoGame'
+
+function App() {
+  return (
+    <div className="App">
+      <BingoGame/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BingoGame/gameData.js
@@ -0,0 +1,136 @@
+
+const gameData = [
+    {
+        label: 'Good Morning',
+        value: 0,
+        disabled: false
+      },
+    {
+      label: 'Günaydın',
+      value: 1,
+      disabled: false
+    },
+    {
+      label: 'Buenos dias ',
+      value: 2,
+      disabled: false
+    },
+    {
+      label: 'Bonjour ',
+      value: 3,
+      disabled: false
+    },
+    {
+        label: 'Guten Morgen',
+        value: 4,
+        disabled: false
+      },
+      {
+        label: 'Buongiorno',
+        value: 5,
+        disabled: false
+      },
+      {
+        label: 'Goedemorgen',
+        value: 6,
+        disabled: false
+      },
+      {
+        label: 'God morgon',
+        value: 7,
+        disabled: false
+      },
+      {
+        label: 'maidin mhaith',
+        value: 8,
+        disabled: false
+      },
+      {
+        label: 'Bom dia',
+        value: 9,
+        disabled: false
+      },
+      {
+        label: 'Magandang umaga po',
+        value: 10,
+        disabled: false
+      },
+      {
+        label: 'Selamat pagi',
+        value: 11,
+        disabled: false
+      },
+      {
+        label: 'sobh bekheir',
+        value: 12,
+        disabled: false
+      },
+      {
+        label: 'Dzien dobry',
+        value: 13,
+        disabled: false
+      },
+      {
+        label: 'Miremengies',
+        value: 14,
+        disabled: false
+      },
+      {
+        label: 'Sabah al Khair',
+        value: 15,
+        disabled: false
+      },
+      {
+        label: 'Sabahınız xeyir',
+        value: 16,
+        disabled: false
+      },
+      {
+        label: 'Safro bricho',
+        value: 17,
+        disabled: false
+      },
+      {
+        label: 'Bonos dies',
+        value: 18,
+        disabled: false
+      },
+      {
+        label: 'xuprobhat',
+        value: 19,
+        disabled: false
+      },
+      
+      {
+        label: 'Dobray ranici',
+        value: 20,
+        disabled: false
+      },
+      {
+        label: 'Mingalarba',
+        value: 21,
+        disabled: false
+      },
+      {
+        label: 'Dobro utro',
+        value: 22,
+        disabled: false
+      },
+      {
+        label: 'Dobro jutro',
+        value: 23,
+        disabled: false
+      },
+      {
+        label: 'Mwjang Fung',
+        value: 24,
+        disabled: false
+      },
+      {
+        label: 'dobry den',
+        value: 25,
+        disabled: false
+      }
+  ]
+
+export {gameData}
